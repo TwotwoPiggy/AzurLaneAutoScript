bb55794909b4366b826f31ed702f40bda90a5918
Lemony <csf2412297817@163.com>
2026-09-11 22:40:20 +0800

    feat: Windows安装器与toolkit环境同步对齐双更新源及分支切换

--- deploy/Windows/config.py
+++ deploy/Windows/config.py
@@ -16,6 +16,11 @@ class ConfigModel:
     # Git
     Repository: str = "https://github.com/LmeSzinc/AzurLaneAutoScript"
     Branch: str = "master"
+    UpdateSource: str = "custom"
+    OfficialRepository: str = "https://github.com/LmeSzinc/AzurLaneAutoScript"
+    OfficialBranch: str = "master"
+    CustomRepository: Optional[str] = "https://github.com/TwotwoPiggy/AzurLaneAutoScript"
+    CustomBranch: str = "custom"
     GitExecutable: str = "./toolkit/Git/mingw64/bin/git.exe"
     GitProxy: Optional[str] = None
     SSLVerify: bool = False
@@ -118,8 +123,59 @@ class DeployConfig(ConfigModel):
         # Bypass webui.config.DeployConfig.__setattr__()
         # Don't write these into deploy.yaml
         super().__setattr__('GitOverCdn', self.Repository in ['cn'])
-        if self.Repository in ['global', 'cn']:
-            super().__setattr__('Repository', 'https://github.com/LmeSzinc/StarRailCopilot')
+        update_source = self.config.get('UpdateSource', getattr(self, 'UpdateSource', 'custom'))
+        official_repo = self.config.get('OfficialRepository', getattr(self, 'OfficialRepository', 'https://github.com/LmeSzinc/AzurLaneAutoScript'))
+        official_branch = self.config.get('OfficialBranch', getattr(self, 'OfficialBranch', 'master'))
+        custom_repo = self.config.get('CustomRepository', getattr(self, 'CustomRepository', 'https://github.com/TwotwoPiggy/AzurLaneAutoScript'))
+        if not custom_repo:
+            custom_repo = 'https://github.com/TwotwoPiggy/AzurLaneAutoScript'
+        custom_branch = self.config.get('CustomBranch', getattr(self, 'CustomBranch', 'custom'))
+        if not custom_branch:
+            custom_branch = 'custom'
+
+        if update_source == 'official':
+            self.Repository = official_repo
+            self.Branch = official_branch
+        elif update_source == 'custom':
+            self.Repository = custom_repo
+            self.Branch = custom_branch
+
+        self.UpdateSource = update_source
+        self.OfficialRepository = official_repo
+        self.OfficialBranch = official_branch
+        self.CustomRepository = custom_repo
+        self.CustomBranch = custom_branch
+
+        if self.Repository in ['global']:
+            super().__setattr__('Repository', 'https://github.com/LmeSzinc/AzurLaneAutoScript')
+        if self.Repository in ['cn']:
+            super().__setattr__('Repository', 'git://git.lyoko.io/AzurLaneAutoScript')
+
+    def switch_source(self, source: str, custom_repo: Optional[str] = None, custom_branch: Optional[str] = None):
+        """
+        Switch active Git update source between 'official' and 'custom'.
+        """
+        self.UpdateSource = source
+        if custom_repo is not None:
+            self.CustomRepository = custom_repo
+            self.config['CustomRepository'] = custom_repo
+        if custom_branch is not None:
+            self.CustomBranch = custom_branch
+            self.config['CustomBranch'] = custom_branch
+
+        if source == 'official':
+            self.Repository = self.OfficialRepository
+            self.Branch = self.OfficialBranch
+        elif source == 'custom' and self.CustomRepository:
+            self.Repository = self.CustomRepository
+            self.Branch = self.CustomBranch
+
+        self.config['UpdateSource'] = self.UpdateSource
+        self.config['OfficialRepository'] = self.OfficialRepository
+        self.config['OfficialBranch'] = self.OfficialBranch
+        self.config['Repository'] = self.Repository
+        self.config['Branch'] = self.Branch
+        self.write()
 
     def filepath(self, path):
         """
